9fd649110c7d63d9fc97cfba6fad79ec55b78b62
Jason Coggon <jcoggon@modernmanagement.co.za>
2023-08-21 11:53:42 +0200

    Updates

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql://qlikuser:qlikpassword@localhost:5432/qlikdb'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://qlikuser:qlikpassword@postgres:5432/qlikdb'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
